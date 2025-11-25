534f3e01f50b1830928f9c7740228c72f5c4b4a1
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-11-25 09:17:12 +0530

    Changed the google-chrome to chromium

--- present.sh
+++ present.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome
+export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
 presenterm ./rust_bangalore.md -x -c ./config/presenterm.yml -p
